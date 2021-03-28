beaf6743df78e40ccc1f016112bae7ef8681134b
Marie Fedinová <marie.fedinova@gmail.com>
2021-03-28 11:40:04 +0200

    házení kostkou

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 // tady je místo pro náš program
+
+function toss() {
+    x = Math.floor(Math.random()* 6 + 1);
+    document.querySelector('#kostka').src = 'obrazky/' + x + '.png';
+    if (x < 6) {
+        document.querySelector('#zprava').textContent = 'Hodilas ' + x + '. Zkus to znovu.';
+    } else {
+        document.querySelector('#zprava').textContent = 'Hodilas ' + x + '. Výhra!';
+    }
+    
+}
